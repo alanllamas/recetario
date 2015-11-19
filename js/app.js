241aa68b3d5d12e1d871fdb76d6298a5e76ceff4
alanllamas <alanllamas@github.org>
2015-11-18 21:58:01 -0600

    se agrega hasta nueva receta

--- js/app.js
+++ js/app.js
@@ -5,15 +5,29 @@ app.controller('myCtrl', function($scope) {
     $scope.recetaNombre="";
     $scope.materias = [];
 	$scope.materia1 = "";
-	$scope.cantidad1 = "";
+	$scope.cantidad1 = 0;
 	$scope.medicion1 = "";
-	$scope.precio1 = "";
+	$scope.pesopza = 0;
+	$scope.precio1 = 0;
 	$scope.meds = ["gr","ml","pza"];
 	$scope.recetas = [];
 	$scope.base = [];
 	$scope.ingrediente = "";
-	$scope.cantidad2 = "";
+	$scope.cantidad2 = 0;
 	$scope.medicion2 = "";
+
+	// hace funcionar los tabs y pills
+	$(function(){ 
+	    $('.nav-pills a').on('click', function (e) {
+	        e.preventDefault();
+	        $(this).tab('show');
+
+	    });  
+	    $('.nav-tabs a').on('click', function (e) {
+	        e.preventDefault();
+	        $(this).tab('show');
+	    }); 
+	});
 
 	
     // agrega el producto a la lista de materias primas
@@ -28,6 +42,13 @@ app.controller('myCtrl', function($scope) {
     		$scope.materia.precio = $scope.precio1;
     		$scope.materia.id = $scope.materias.length + 1;
 
+    		// le da un peso a la materia prima si se mide por piezas
+    		if ($scope.materia.medicion == "pza") {
+    			
+    			$scope.materia.pesopza = $scope.pesopza ;
+    			console.log($scope.materia);
+    		};
+
     		// evita que haya entradas iguales
     		if ($scope.materias.length == 0) {
     			$scope.materias.push($scope.materia);
@@ -37,7 +58,7 @@ app.controller('myCtrl', function($scope) {
 	    		
 	    			if ($scope.materia.materia == $scope.materias[i].materia) {
 	    				
-	    				counter +=1;
+	    				counter++;
 	    			};
 	    			
 	    		};
@@ -52,19 +73,46 @@ app.controller('myCtrl', function($scope) {
 		};
 	});
 
-			$scope.receta = [];
+			$scope.receta = {};
+			$scope.receta.id;
+			$scope.receta.nombre = "";
+			$scope.receta.ingredientes = [];
+			$scope.receta.costo = 0;
+			$scope.receta.cantidad = 0;
+			
 	$('#agregarIngrediente').click(function(event) {
-		if ($scope.ingrediente && $scope.cantidad2 && $scope.medicion2 !== "") {
+		ing = $scope.receta.ingredientes;
+		if ($scope.ingrediente && $scope.cantidad2  !== "") {
 
 			$scope.ingre = {};
+			$scope.ingre.id = $scope.receta.ingredientes.length +1 ;
 			$scope.ingre.materia = $scope.ingrediente.materia;
 			$scope.ingre.cantidad = $scope.cantidad2;
-			$scope.ingre.medicion = $scope.medicion2;
+			$scope.ingre.medicion = $scope.ingrediente.medicion;
 
+			// si hay un peso por pieza lo agrega
+
+			if ($scope.ingrediente.pesopza) {
+				$scope.ingre.pesopza = $scope.ingrediente.pesopza;
+			};
+			// obtiene el precio segun la cantidad del producto
+			$scope.ingre.precio = function () {
+				
+				precio = $scope.ingre.cantidad * $scope.ingrediente.precio / $scope.ingrediente.cantidad;
+
+				return precio;
+
+			}();
+
+			// agrega el ingrediente al array de ingrediente
 			if ($scope.receta.length == 0) {
-    			$scope.receta.push($scope.materia);
+
+    			ing.push($scope.ingre);
+    		// evita que haya entradas repetidas
     		}else{
+
     			counter = 0;
+
 	    		for (var i = 0; i < $scope.receta.length; i++) {
 	    		
 	    			if ($scope.ingre.materia == $scope.receta[i].materia) {
@@ -76,24 +124,70 @@ app.controller('myCtrl', function($scope) {
 	    			
 	    		if (counter == 0) {
 					
-					$scope.receta.push($scope.ingre);
+					ing.push($scope.ingre);
 
 	    		};
     		};
-			//falta hacer el costeador en esta parte
+
+	    	$scope.receta.id = $scope.recetas.length + 1 ;
+
+			//suma la cantidad total de la receta
+			
+			$scope.receta.cantidad = (function(){
+				var cantidadTotal = 0;
+				for (var i = 0; i < $scope.receta.ingredientes.length; i++) {
+						console.log($scope.receta.ingredientes[i]);
+						console.log($scope.receta.ingredientes[i].cantidad);
+					if ($scope.receta.ingredientes[i].medicion == "pza") {
+						console.log($scope.receta.ingredientes[i].pesopza);
+						cantidad = $scope.receta.ingredientes[i].cantidad * $scope.receta.ingredientes[i].pesopza;
+					}else{
+						
+						cantidad = Number($scope.receta.ingredientes[i].cantidad);
+						console.log(typeof(cantidad));
+					// cantidad = cantidad;
+					};
+					cantidadTotal += cantidad;
+
+				};
+				return cantidadTotal;
+			})();
+
+			// suma el costo total de la receta
+
+			$scope.receta.costo = (function(){
+				var costo = 0,
+					costoTotal = 0;
+				for (var i = 0; i < $scope.receta.ingredientes.length; i++) {
+
+						
+						console.log($scope.receta.ingredientes[i]);
+						console.log($scope.receta.ingredientes[i].precio);
+						costo += $scope.receta.ingredientes[i].precio;
+						console.log(costo);
+						costoTotal += costo;
+						console.log(costoTotal);
+
+				};
+				return costoTotal;
+			})();
 		};
 	});
-
 		// esconde el input del nombre de la receta
     $('#Receta').blur(function(event) {
     	if ($scope.recetaNombre != "") {
+    		$scope.receta.nombre = $scope.recetaNombre;
     		$('#Receta').hide();
     	};
     	event.preventDefault();
     });
     $('#agregarReceta').click(function(event) {
     	$scope.recetas.push($scope.receta)
-    	console.log($scope.recetas);
+    	for (var i = 0; i < $scope.recetas.length; i++) {
+    		$scope.recetas[i]
+    	console.log($scope.recetas[i]);
+    	console.log($scope.recetas[i].id);
+    	};
     });
 
 
